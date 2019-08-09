65c56ac4bb2f766fcf823ce7e148e5aee28c9ecc
Evgeniy <evgen19-97@ya.ru>
2019-08-09 15:45:14 +0300

    init train script

---
+++ neural_network_trainig/trainModel.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class NeuralNetwork(nn.Module):
+    pass
+
+    def __init__(self):
+        super(NeuralNetwork,self).__init__()
+        pass
+
+    def forward(self,x):
+        pass
+
+def train():
+    pass
+
+def test():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
