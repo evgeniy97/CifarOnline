ebcc57701a2cdf0ae0a0e27e25faa31ca7f045a7
Evgeniy <evgen19-97@ya.ru>
2019-08-09 15:54:51 +0300

    init app prototype

---
+++ flask_app/app.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from flask import Flask, request, jsonify, Response
+
+app = Flask(__name__)
+
+def loadModel():
+    pass
+
+
+def preproccesImage():
+    pass
+
+@app.route('/imports',methods=['POST'])
+def post():
+    pass
+
+if __name__ == '__main__':
+    app.run()
